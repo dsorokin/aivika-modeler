063f1943df53d86809a269c386f1d29893522d3f
David Sorokin <david.sorokin@gmail.com>
2017-07-22 15:29:51 +0300

    Fixed module files.

---
+++ simulation/aivika/modeler/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2017 David Sorokin <david.sorokin@gmail.com>
+#
+# Licensed under BSD3. See the LICENSE.txt file in the root of this distribution.
+
+from simulation.aivika.modeler.model import *
+from simulation.aivika.modeler.port import *
