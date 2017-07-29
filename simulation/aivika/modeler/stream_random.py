52b301bdb3ae6b912057472d419b35e30cb852f3
David Sorokin <david.sorokin@gmail.com>
2017-07-29 15:55:42 +0300

    Added more random streams.

--- simulation/aivika/modeler/stream_random.py
+++ simulation/aivika/modeler/stream_random.py
@@ -39,3 +39,29 @@ def triangular_random_stream(transact_type, min_delay, median_delay, max_delay):
     y.bind_to_input()
     y.write(code)
     return y
+
+def normal_random_stream(transact_type, mean_delay, delay_deviation):
+    """Return a new stream of transacts with random delays having the normal distribution."""
+    expect_transact_type(transact_type)
+    model = transact_type.get_model()
+    code = 'return $ mapStream (\\a -> ' + transact_type.coerce_arrival('a') + ') $ '
+    code += 'randomNormalStream ' + str(mean_delay) + ' ' + str(delay_deviation)
+    y = StreamPort(model, transact_type.get_data_type())
+    y.bind_to_input()
+    y.write(code)
+    return y
+
+def lognormal_random_stream(transact_type, normal_mean_delay, normal_delay_deviation):
+    """Return a new stream of transacts with random delays having the lognormal distribution.
+
+       The numerical parameters are related to the normal distribution that
+       this distribution is derived from.
+    """
+    expect_transact_type(transact_type)
+    model = transact_type.get_model()
+    code = 'return $ mapStream (\\a -> ' + transact_type.coerce_arrival('a') + ') $ '
+    code += 'randomLogNormalStream ' + str(normal_mean_delay) + ' ' + str(normal_delay_deviation)
+    y = StreamPort(model, transact_type.get_data_type())
+    y.bind_to_input()
+    y.write(code)
+    return y
