849a57990c454da5927c270eaf5c8a30d84556ac
David Sorokin <david.sorokin@gmail.com>
2017-07-30 17:35:35 +0300

    Updated the license description.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Topic :: Scientific/Engineering',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD3 License',
+        'License :: OSI Approved :: BSD License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
